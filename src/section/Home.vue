1faec1e2204b5696aae56ae586fdf2a713b0e786
Darrel Toledo <darreltoledo@icloud.com>
2021-07-08 21:18:26 +0100

    width and height

--- src/section/Home.vue
+++ src/section/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    
+  <div id="home">
   </div>
 </template>
+<script>
+export default {
+  //
+}
+</script>
+
+<style lang="scss" scoped>
+#home {
+  width: 100%;
+  height: 100vh;
+}
+</style>>

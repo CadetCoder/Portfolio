6a07dcb4c2b44c1572dfeeaf74bd65448914474b
Darrel Toledo <darreltoledo@icloud.com>
2021-07-12 20:33:40 +0100

    hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <div class="hero-text">
-      
+      <h1 ref="greeting">Hi I'm</h1>
+      <h2 ref="name">Darrel.</h2>
+      <div ref="profession" @click="goToMe" class="title-wrapper">
+        <h3>Web Developer</h3>
+      </div>
     </div>
   </main>
 </template>

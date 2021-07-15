656afde0d92c3319a21480a60ee541cb18a29559
Darrel Toledo <darreltoledo@icloud.com>
2021-07-15 22:15:23 +0100

    template img

--- src/components/FindMeIcons.vue
+++ src/components/FindMeIcons.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="find-me-icons-main-container"
   @click="openLink">
-
+    <div class="fine-me-icons-header">
+      <img :src="require(`../assets/icons/${title}-white.svg`)" class="horizontal-divider">
+    </div>
   </div>
 </template>

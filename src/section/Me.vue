0b4834d5268a29977dc1908fb155b082f4c91bdf
Darrel Toledo <darreltoledo@icloud.com>
2021-07-21 22:00:35 +0100

    set a backrgound image

--- src/section/Me.vue
+++ src/section/Me.vue
@@ -7,3 +7,13 @@ export default {
   components: {}
 }
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url("../assets/patterns/topography.svg");
+}
+</style>

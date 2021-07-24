772d5ebf141863439d43239cc1aab073234ce22a
Darrel Toledo <darreltoledo@icloud.com>
2021-07-24 21:56:56 +0100

    image and h1 name

--- src/section/FooterCurve.vue
+++ src/section/FooterCurve.vue
@@ -2,6 +2,8 @@
   <div>
     <footer>
       <img src="../assets/logo/logo-dark.svg" alt="dtoledo logo" />
+      <div class="vertical-divider"></div>
+      <h1>Darrel Toledo</h1>
     </footer>
   </div>
 </template>

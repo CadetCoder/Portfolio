9134f5fe4d192c4266c03f1ac8880d3da2fcf0ec
Darrel Toledo <darreltoledo@icloud.com>
2021-07-28 22:16:39 +0100

    adding description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -3,6 +3,13 @@
     <h2>A little about me</h2>
     <p>
       I am a Web Developer with experience in building responsive websites and web applications while keeping the aesthetic integrity.
+      Experienced in semantic
+      <span
+        ref="reactive"
+        @mouseenter="reactiveHighlight"
+        @mouseleave="reactiveHighlightRemove"
+        >HTML, CSS, JavaScript</span
+      >
     </p>
   </article>
 </template>

5bcb84dcdbc32337d7a828177de8b04ac0954f88
Darrel Toledo <darreltoledo@icloud.com>
2021-07-31 21:52:25 +0100

    adding div style

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -9,3 +9,14 @@ export default {
   name: "Resume",
 };
 </script>
+
+
+<style scoped>
+div {
+  height: 50px;
+  width: 50px;
+  background-color: white;
+  border-radius: 50%;
+  padding: 0.5em;
+}
+</style>

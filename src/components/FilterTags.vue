a9fbe237645748f611501d55a91a9cb75a6fc532
Darrel Toledo <darreltoledo@icloud.com>
2021-08-01 21:46:10 +0100

    type

--- src/components/FilterTags.vue
+++ src/components/FilterTags.vue
@@ -1,3 +1,8 @@
 <template>
-  <div></div>
+  <div
+    @click="$emit('clicked', type)"
+    :class="state ? 'type-meta-active' : 'type-meta'"
+  >
+    {{ type }} <img v-if="type === 'Favorites'" src="../assets/icons/star.svg" alt="favorites" />
+  </div>
 </template>

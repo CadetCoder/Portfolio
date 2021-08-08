1f9d0c73c263cd5280abb5d6c9bcc36c9c8f65df
Darrel Toledo <darreltoledo@icloud.com>
2021-08-08 22:02:27 +0100

    icons

--- src/components/ProjectModule.vue
+++ src/components/ProjectModule.vue
@@ -5,6 +5,21 @@
         <h1></h1>
         <div class="type-meta"></div>
       </div>
+      <div class="custom-icons">
+        <a
+          v-for="(link, platform) in preview"
+          :key="platform.index"
+          :href="link"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            class="preview-platform-icons"
+            :src="require(`../assets/icons/${platform}.svg`)"
+            :alt="`link for ${platform}`"
+          />
+        </a>
+      </div>
     </header>
   </article>
 </template>

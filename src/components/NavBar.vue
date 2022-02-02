034c74b2f5e78c7dfc231bd1ce850a5017231eb0
Darrel Toledo <darreltoledo@icloud.com>
2022-02-02 22:27:55 +0000

    added navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,13 @@
 <template>
-  <v-app-bar></v-app-bar>
+  <v-app-bar flat absolute color="transparent" dark height="120px">
+    <v-layout>
+      <v-container>
+        <v-layout>
+          <div
+            :class="{'display-1 font-weight-bold': $vuetify.breakpoint.smAndUp,'title font-weight-bold': $vuetify.breakpoint.smAndDown, }"
+          >Darrel T.</div>
+          <v-spacer></v-spacer>
+      </v-container>
+    </v-layout>
+  </v-app-bar>
 </template>

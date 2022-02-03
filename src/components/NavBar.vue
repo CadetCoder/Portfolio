f7222a88ab5ef53103bbab666c59122a0e76027d
Darrel Toledo <darreltoledo@icloud.com>
2022-02-03 18:16:45 +0000

    Nevbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,39 @@
             :class="{'display-1 font-weight-bold': $vuetify.breakpoint.smAndUp,'title font-weight-bold': $vuetify.breakpoint.smAndDown, }"
           >Darrel T.</div>
           <v-spacer></v-spacer>
+          <v-toolbar-items class="text-right">
+            <v-btn
+              :x-small="$vuetify.breakpoint.smAndDown"
+              @click="$vuetify.goTo('#about',options )"
+              text
+            >About</v-btn>
+            <v-btn
+              :x-small="$vuetify.breakpoint.smAndDown"
+              @click="$vuetify.goTo('#portfolio',options )"
+              text
+            >My Work</v-btn>
+            <v-btn
+              :x-small="$vuetify.breakpoint.smAndDown"
+              @click="$vuetify.goTo('#contact',options )"
+              text
+            >Contact</v-btn>
+          </v-toolbar-items>
+        </v-layout>            
       </v-container>
     </v-layout>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        duration: 300,
+        offset: 0,
+        easing: "easeInOutCubic"
+      }
+    }
+  }
+}
+</script>

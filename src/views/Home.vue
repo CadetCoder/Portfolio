4d255d7f94a98a6d97c85a34e9ab455161598f89
Darrel Toledo <darreltoledo@icloud.com>
2022-02-04 09:53:04 +0000

    added bg image on landing page & remvd dfault home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-img 
+    dark
+    max-height="500px"
+    src="/Users/Darrel/Desktop/Portfolio/src/assets/one.jpg"></v-img>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    //
   }
 }
 </script>

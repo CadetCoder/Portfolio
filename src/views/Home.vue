25e293869e8c1111a6dd32457e0fefbebffb6b6e
Darrel Toledo <darreltoledo@icloud.com>
2022-02-05 09:46:09 +0000

    added welcome page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,22 @@
     dark
     max-height="500px"
     src="/Users/Darrel/Desktop/Portfolio/src/assets/one.jpg"></v-img>
+
+    <v-layout fill-height align-center>
+        <v-container>
+          <v-row align="center" justify="center" class="white--text">
+            <v-col cols="12">
+              <div
+                :class="{'display-2 font-weight-bold ': $vuetify.breakpoint.smAndUp, 'title font-weight-bold': $vuetify.breakpoint.smAndDown}"
+              >Welcome to my page.</div>
+              <div
+                :class="{'headline': $vuetify.breakpoint.smAndUp, 'subtitle-1': $vuetify.breakpoint.smAndDown}"
+              >I make applications.</div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-layout>
+      
   </div>
 </template>
 

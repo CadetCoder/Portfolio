76695966882abe130f4a6801163c2f12fd736354
Darrel Toledo <darreltoledo@icloud.com>
2022-02-09 21:28:41 +0000

    added about text and data script

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,6 +33,22 @@
           >Working on starting an app development business and always looking for new clients! Would love to talk to you about your idea.</div>
         </v-flex>
       </v-layout>
+
+       <v-layout wrap my-5>
+        <v-flex xs12>
+          <div class="display-1 font-weight-bold" id="portfolio">My Work</div>
+        </v-flex>
+        <v-flex xs12 class="mt-5">
+          <v-layout wrap>
+            <template v-for="(app, i) in apps">
+              <v-flex :key="i" xs12 sm6 md4>
+                <p-portfolio-app :app="app" />
+              </v-flex>
+            </template>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
     </v-container>
 
   </div>
@@ -46,6 +62,13 @@ export default {
   name: 'Home',
   components: {
     //
+  },
+  data(){
+    return {
+      aboutText:
+        "I enjoy building apps in my free time when I'm not doing much. Looking to get into machine learning and trying to automate as many things as possible. Love to encourage new coders that they can do it too! I'm not very smart, just curious to see how things work. I'm also not that good of a writer as you can see. lol. Anyways, you are free to download any of the code thats on my github! And I hope you have a great day!",
+        
+    }
   }
 }
 </script>

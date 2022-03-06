a51591a4fde5d4dcbaf3944e18284443b2921085
Darrel Toledo <darreltoledo@icloud.com>
2022-03-06 22:12:00 +0000

    debug header

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header .container');
+const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click',()=> {
   hamburger.classList.toggle('active');
@@ -8,10 +8,10 @@ hamburger.addEventListener('click',()=> {
 });
 
 document.addEventListener('scroll', () => {
-  var scroll_position = window.scrollY;
-  if (scroll_position > 250) {
-    header.style.backgroundColor = '#29323c';
-  } else {
-    header.style.backgroundColor = 'transparent';
-  }
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
 });
